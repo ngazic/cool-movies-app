134ffd039647a4939a0561c3d0baabb6b19778d7
Nihad <ngazic@gmail.com>
2021-06-17 08:48:04 +0200

    fixing api errors for fetching http

--- src/API-helper.ts
+++ src/API-helper.ts
@@ -1,6 +1,6 @@
 const API_SETTINGS = {
-  apiBasePath: 'http://www.omdbapi.com/?apikey',
-  apiImgPath: 'http://img.omdbapi.com/?apikey',
+  apiBasePath: 'https://www.omdbapi.com/?apikey',
+  apiImgPath: 'https://img.omdbapi.com/?apikey',
   REACT_APP_API_KEY: 'd1109680'
 }
 
